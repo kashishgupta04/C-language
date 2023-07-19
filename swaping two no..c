b3965436349ede8f88af45e5dc167210d6d38423
Kashish Gupta <138710213+kashishgupta04@users.noreply.github.com>
2023-07-19 19:54:00 +0530

    Add files via upload

---
+++ swaping two no..c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d;
+    printf("Enter the two numbers:");
+    scanf("%d %d",&a,&b);
+    c=b;
+    d=a;
+    printf("swaping values are:%d %d",c,d);
+
+    
+
+
+    return 0;
+}
