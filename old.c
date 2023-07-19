befa8110890dad7e015258b7365017aee12059eb
Kashish Gupta <138710213+kashishgupta04@users.noreply.github.com>
2023-07-19 20:06:04 +0530

    Add files via upload

---
+++ old.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a;
+    printf("Enter age:");
+    scanf("%d",&a);
+    (a>50)?printf("Senior Citizen"):printf("not a senior citizen");
+    return 0;
+}
