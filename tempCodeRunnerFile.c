4a8367fb51eb53aa254ec02968d646675b853d3c
Kashish Gupta <138710213+kashishgupta04@users.noreply.github.com>
2023-07-24 14:13:46 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int l,b;
+    printf("Length is:");
+    scanf("%d",&l);
+    printf("\nBreath is:");
+    scanf("%d",&b);
+    (l==b)? printf("Cardboard is sqaure"): printf("Cardboard is rectangle");
+
+
+    return 0;
+}
